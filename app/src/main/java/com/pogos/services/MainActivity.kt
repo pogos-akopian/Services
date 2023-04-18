203e52e677158be3f4b4ab748f4c98635fc5d674
pogos-akopian <sofinmern@gmail.com>
2023-04-18 22:05:45 +0700

    project created //+dependencies //+viewBinding

---
+++ app/src/main/java/com/pogos/services/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.pogos.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pogos.services.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
